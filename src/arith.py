274df89880198c90aa6cd9fa2210aaa9d9a2ec71
Tianguang <penn.zht@gmail.com>
2022-11-04 00:29:51 +0100

    Add first successful theory (1+1).

--- src/arith.py
+++ src/arith.py
@@ -93,22 +93,22 @@ def mergematch (m1, m2):
             return False
     return ans
 
-def match (form, pattern):
+def _primitive_match (form, pattern):
     '''Returns whether [form] matches [pattern].
 
     Returns a nonempty object if match succeeds, and
     [False] if match fails.'''
-    if isseq (pattern) and len (pattern) == 2 and pattern[0].startswith('**'):
+    if isseq (pattern) and len (pattern) == 2 and type (pattern[0]) is str and pattern[0].startswith('**'):
         # Application pattern
         # TODO: use delayed match; afterprocessing for match
-        pass # TODO
+        return {pattern: form}
 
     if isseq (pattern):
         if not (isseq (form) and len (form) == len (pattern)):
             return False
         ans = {1: True}
         for (subf, subp) in zip (form, pattern):
-            subm = match (subf, subp)
+            subm = _primitive_match (subf, subp)
             ans = mergematch (ans, subm)
             if not ans:
                 return False
@@ -122,6 +122,83 @@ def match (form, pattern):
             return {1: True}
         else:
             return False
+
+def _attempt_resolve (a, b, pa, pb):
+    if pa == pb:
+        return pa
+    if a == pa and b == pb:
+        return '*_'
+    if type (pa) == type (pb) == tuple and len (pa) == len (pb):
+        partial_matches = [_attempt_resolve (a, b, parta, partb)
+                           for (parta, partb) in zip (pa, pb)]
+        if None in partial_matches:
+            return None
+        return tuple (partial_matches)
+    return None
+
+def match (form, pattern):
+    form = tuple (form)
+    pattern = tuple (pattern)
+    pm_match = _primitive_match (form, pattern)
+
+    atomic_matches = {} # str -> form
+    pattern_matches = {} # **p -> [(str, form) *]
+    for p, f in pm_match.items ():
+        if type (p) is str and p.startswith ('*'):
+            atomic_matches[p] = f
+        elif type (p) is tuple and type (p[0]) is str and p[0].startswith ('**'):
+            (phead, pvar) = p
+            pattern_matches.setdefault (phead, [])
+            pattern_matches[phead].append ((pvar, f))
+        elif p == 1:
+            pass # Placeholder pattern
+        else:
+            raise Exception (f'Bad pattern {p}')
+    # TODO: Handle pattern matches
+    # How?
+    # match (a, b, pa, pb) is true if and only if:
+    #    pa == pb
+    # || a == pa && b == pb
+    # || len (pa) == len (pb) && match (a, b, pa[i], pb[i]) for all i
+    # What about missing parameters?
+    #     Currently: no.
+    resolved = {}
+    for p, p_matches in pattern_matches.items ():
+        # Check one-output goals
+        if all (x[1] == p_matches[0][1] for x in p_matches):
+            ans = p_matches[0][1]
+            resolved[p] = ans
+            continue
+        # Unify goals with same input
+        unified_goals = {}
+        for (a, pa) in p_matches:
+            if type (a) is str and a.startswith ('*'):
+                if a in atomic_matches:
+                    norm = atomic_matches[a]
+                else:
+                    continue
+            else:
+                norm = a
+            # Add norm -> pa goal
+            if norm not in unified_goals:
+                unified_goals[norm] = pa
+            elif unified_goals[norm] != pa:
+                return False # No match
+        # Otherwise, enter match
+        goals = list (unified_goals.items ())
+        for i in range (1, len (goals)):
+            (a, pa) = goals[0]
+            (b, pb) = goals[i]
+            resolution_2 = _attempt_resolve (a, b, pa, pb)
+            if resolution_2 is None:
+                return False # No match
+            elif i == 1:
+                resolution = resolution_2
+            elif resolution != resolution_2:
+                return False # No match
+        # Successful match!
+        resolved[p] = resolution
+    return {*atomic_matches, *resolved}
 
 # Statements
 # (name, (origin-name, origin-parts), stmt)
@@ -162,16 +239,17 @@ AXIOMS = {
     'peano-0':    expr.parseall ('(= (S *a) (S *b)) (= *a *b)'),
     'peano-1':    expr.parseall ('(not (= (S *a) O))'),
     'peano-2':    expr.parseall ('(**p O) (forall (=> nat n (impl (**p n) (**p (S n))))) (forall (=> nat n (**p n)))'),
-    '+-0':        expr.parseall ('(= (+ *a O) *a)'),
+    '+-O':        expr.parseall ('(= (+ *a O) *a)'),
     '+-S':        expr.parseall ('(= (+ *a (S *b)) (S (+ *a *b)))'),
-    '*-0':        expr.parseall ('(= (* *a O) O)'),
+    '*-O':        expr.parseall ('(= (* *a O) O)'),
     '*-S':        expr.parseall ('(= (* *a (S *b)) (+ *a (* *a *b)))'),
-    '^-0':        expr.parseall ('(= (^ *a O) (S O))'),
+    '^-O':        expr.parseall ('(= (^ *a O) (S O))'),
     '^-S':        expr.parseall ('(= (^ *a (S *b)) (* *a (^ *a *b)))'),
 }
 
 def is_valid_derivation (axiom, sentences):
     # TODO: Add 'assumption', 'weakening', 'contraction'
+    # Actually, only 'assumption' is necessary.
     if axiom == 'and-i':
         return match (sentences, ['*a', '*b', ('and', '*a', '*b')])
     elif axiom == 'and-el':
@@ -246,16 +324,16 @@ def is_valid_derivation (axiom, sentences):
                                   ('forall', ('=>', 'nat', 'n', ('impl', ('**p', 'n'),
                                                                  ('**p', ('S', 'n'))))),
                                   ('forall', ('=>', 'nat', 'n', ('**p', 'n')))])
-    elif axiom == '+-0':
+    elif axiom == '+-O':
         return match (sentences, [('=', ('+', '*a', 'O'), '*a')])
     elif axiom == '+-S':
         return match (sentences, [('=', ('+', '*a', ('S', '*b')), ('S', ('+', '*a', '*b')))])
-    elif axiom == '*-0':
+    elif axiom == '*-O':
         return match (sentences, [('=', ('*', '*a', 'O'), 'O')])
     elif axiom == '*-S':
         return match (sentences, [('=', ('*', '*a', ('S', '*b')),
                                    ('+', '*a', ('*', '*a', '*b')))])
-    elif axiom == '^-0':
+    elif axiom == '^-O':
         return match (sentences, [('=', ('^', '*a', 'O'), ('S', 'O'))])
     elif axiom == '^-S':
         return match (sentences, [('=', ('^', '*a', ('S', '*b')),
