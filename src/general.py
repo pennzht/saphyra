83195858dc4c7f46f4a451cfdf909020dabfe654
Tianguang <penn.zht@gmail.com>
2023-03-26 15:31:38 +0200

    Add generalized checker.

--- src/general.py
+++ src/general.py
@@ -176,43 +176,12 @@ def match (form, pattern):
 # are `impl-i` and `forall-i`.
 
 # TODO: extract axioms!
+
+_axioms_arith = expr.data ('arith.blue')
+
 AXIOMS = {
-    'and-i':      expr.parseall ('*a *b (and *a *b)'),
-    'and-el':     expr.parseall ('(and *a *b) *a'),
-    'and-er':     expr.parseall ('(and *a *b) *b'),
-
-    'or-il':      expr.parseall ('*a (or *a *b)'),
-    'or-ir':      expr.parseall ('*b (or *a *b)'),
-    'or-e':       expr.parseall ('(or *a *b) (impl *a *c) (impl *b *c) *c'),
-
-    'true-i':     expr.parseall ('true'),
-    'false-e':    expr.parseall ('(impl false *a)'),
-
-    'not-i':      expr.parseall ('(impl *a false) (not *a)'),
-    'not-e':      expr.parseall ('(not *a) (impl *a false)'),
-
-    'impl-e':     expr.parseall ('(impl *a *b) *a *b'),
-
-    'equiv-el':   expr.parseall ('(equiv *a *b) (impl *a *b)'),
-    'equiv-er':   expr.parseall ('(equiv *a *b) (impl *b *a)'),
-
-    'forall-e':   expr.parseall ('(forall (=> nat *n (**p *n))) (**p *m)'),
-
-    'exists-i':   expr.parseall ('(**p *m) (exists (=> nat *n (**p *n)))'),
-    'exists-e':   expr.parseall ('(exists (=> nat *n (**p *n))) (forall (=> nat *n (impl (**p *n) *q))) *q'),
-
-    '=-i':        expr.parseall ('(= *a *a)'),
-    '=-e':        expr.parseall ('(= *a *b) (**p *a) (**p *b)'),
-
-    'peano-0':    expr.parseall ('(= (S *a) (S *b)) (= *a *b)'),
-    'peano-1':    expr.parseall ('(not (= (S *a) O))'),
-    'peano-2':    expr.parseall ('(**p O) (forall (=> nat n (impl (**p n) (**p (S n))))) (forall (=> nat n (**p n)))'),
-    '+-O':        expr.parseall ('(= (+ *a O) *a)'),
-    '+-S':        expr.parseall ('(= (+ *a (S *b)) (S (+ *a *b)))'),
-    '*-O':        expr.parseall ('(= (* *a O) O)'),
-    '*-S':        expr.parseall ('(= (* *a (S *b)) (+ *a (* *a *b)))'),
-    '^-O':        expr.parseall ('(= (^ *a O) (S O))'),
-    '^-S':        expr.parseall ('(= (^ *a (S *b)) (* *a (^ *a *b)))'),
+    _axioms_arith[i]: _axioms_arith[i+1]
+    for i in range (0, len (_axioms_arith), 2)
 }
 
 def is_valid_derivation (axiom, sentences):
