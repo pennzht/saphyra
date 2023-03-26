c399942ad10bdb30e40960ae05e77884ff4e9b50
Tianguang <penn.zht@gmail.com>
2023-03-26 15:54:02 +0200

    Verified.

--- src/general.py
+++ src/general.py
@@ -1,4 +1,4 @@
-# Checks an arithmetic theory.
+# Checks an arithmetic theory, generalized.
 
 # Env items
 # def
@@ -21,6 +21,7 @@
 # (def <type> <def-symbol> <definition>)
 
 import expr
+import sys
 
 Types = {
     'stmt': ['true', 'false'],
@@ -227,4 +228,45 @@ def is_valid_derivation (axiom, sentences):
 
     else:
         raise SyntaxError (f'Invalid axiom: {axiom} @ {sentences}')
+
+class ProofError (Exception):
+    def __init__ (self, claim): self.claim = claim
+    def __repl__ (self): return f'ProofError: {self.claim}'
+
+class ProofSyntaxError (Exception):
+    def __init__ (self, claim): self.claim = claim
+    def __repl__ (self): return f'ProofSyntaxError: {self.claim}'
+
+if __name__ == '__main__':
+    source = sys.stdin.read ()
+    parsed = expr.parseall (source)
+    claims = {}
+    for row in parsed:
+        if type (row) is tuple and row and row[0] == 'comment':
+            continue # Just a comment.
+        try:
+            (_prove, label, axiom_invocation, *environment, _turnstile, result) = row
+            assert _prove == 'prove'
+            assert _turnstile == '|-'
+            assert type (label) is str
+            assert type (axiom_invocation) is tuple
+            assert label not in claims
+            # Construct statement
+            stmt = (tuple (environment), result)
+
+            # Verify statement
+            (axiom, *precedents) = axiom_invocation
+            precedents = [claims[name] for name in precedents]
+            if is_valid_derivation (axiom, [*precedents, stmt]):
+                'success'
+                claims[label] = stmt
+            else:
+                raise ProofError (row)
+        except ValueError:
+            raise ProofSyntaxError (row)
+        except AssertionError:
+            raise ProofSyntaxError (row)
+    # Success!
+    print (f'{len (claims)} claims verified.')
+
 
