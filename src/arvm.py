da146c73995ae036eca713be6e6d64195be7d2bc
Tianguang <penn.zht@gmail.com>
2023-04-16 01:31:10 +0200

    Add first working arvm (lightweight).

--- src/arvm.py
+++ src/arvm.py
@@ -1,5 +1,8 @@
 # A reasonable virtual machine.
 
+import functools
+import operator
+
 '''
 Design
 
@@ -16,6 +19,23 @@ Stack structure
 each frame is: ["F" function value-list active-value-index] or ["V" value] or ["E" expression]
 '''
 
+def expand_function (fn, value_list):
+    if fn == '+':
+        return sum (value_list)
+    elif fn == '*':
+        return functools.reduce (operator.mul, value_list, 1)
+    elif fn == '>':
+        return value_list[0] > value_list[1]
+    elif fn == 'if':
+        a, b, c = value_list
+        return b if a else c
+    raise Exception (f'Unidentified function {fn}')
+
+def replace_index (l, ind, value):
+    l = list (l)
+    l[ind] = value
+    return tuple (l)
+
 def step (stack):
     stack = list (stack)
     tail = stack[-1]
@@ -24,18 +44,24 @@ def step (stack):
         if len (stack) > 1:
             value = tail[1]
             stack.pop ()
-            _, _, value_list, active_index = stack[-1]
-            stack[-1][2][active_index] = value
-            stack[-1][3] += 1
+            _f, fn_name, value_list, active_index = stack.pop ()
+            stack.append ((_f, fn_name, replace_index (value_list, active_index, value), active_index+1))
         else: pass
     elif tail_type == 'F':
         _, fn, value_list, active_index = stack[-1]
         if active_index < len (value_list):
-            stack.push (('E', value_list[active_index]))
+            stack.append (('E', value_list[active_index]))
         else:
             stack.pop ()
-            stack.push (('E', expand_function (fn, value_list)))
+            stack.append (('E', expand_function (fn, value_list)))
     elif tail_type == 'E':
-        _, expr = stack[-1]
-        ... # TODO
+        _, expr = stack.pop ()
+        if isinstance (expr, tuple):
+            # Function application
+            stack.append (('F', expr[0], expr[1:], 0))
+        else:
+            stack.append (('V', int (expr)))
     return tuple (stack)
+
+def initstack (value):
+    return (('E', value),)
--- src/arvm_test.py
+++ src/arvm_test.py
@@ -1,8 +1,21 @@
+import expr
+import arvm
+
 expressions = [
     '(+ 1 2)',
     '(+ (* 3 4) (+ 1 2))',
     '(if (> 2 1) 12 34)',
-    '(globals [(x) 3] (x))',
-    '(globals [(f n) (* n n n)] (f 5))',
+#    '(globals [(x) 3] (x))',
+#    '(globals [(f n) (* n n n)] (f 5))',
 ]
+
+for e in expressions:
+    v = expr.parse (e)
+    s = arvm.initstack (v)
+    print (s)
+    while s != arvm.step (s):
+        s = arvm.step (s)
+        print (s)
+    print ()
+
 
