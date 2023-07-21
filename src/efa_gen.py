364b4efe7e7db5df7bf6ca1ac38f91e18a727fee
Tianguang <penn.zht@gmail.com>
2023-07-21 18:35:27 +0200

    Add successful case of applying a theorem at a location. (Change made on vacation.)

--- src/efa_gen.py
+++ src/efa_gen.py
@@ -38,9 +38,26 @@ def match_pattern (pattern, form):
         else:
             return False
 
+def _lambda_replace_all (exp, replacements):
+    for (var, arg) in replacements.items ():
+        exp = lambdas.lambda_replace (exp, var, arg)
+    return exp
+
 def apply_theorem_at (theorem_left, theorem_right, term, path):
     subterm = lambdas.sub_by_path (term, path)
     attempt_match = match_pattern (pattern = theorem_left, form = subterm)
+    if not attempt_match:
+        return 'Cannot apply theorem.'
+    reconstructed_subterm = _lambda_replace_all(
+        theorem_left,
+        attempt_match)
+    if not lambdas.lambda_eq (reconstructed_subterm, subterm):
+        return 'Cannot apply theorem: replacement failed.'
+    # Replacement successful.
+    new_subterm = _lambda_replace_all(
+        theorem_right,
+        attempt_match)
+    return lambdas.replace_by_path (term, path, new_subterm)
     pass  # TODO, continue
 
 if __name__ == '__main__':
@@ -60,4 +77,10 @@ if __name__ == '__main__':
         expr.parse ('((_a + (S _b)) = (S (_a + _b)))'),
         expr.parse ('((_O + (S _O)) = (S (_O + _O)))'),
     ))
+    print (apply_theorem_at (
+        expr.parse ('(if (S _c) _a _b)'),
+        expr.parse ('_a'),
+        expr.parse ('((if (S _r) (S _r) O) 1 2)'),
+        (0,),
+    ))  # Successful: ((S _r) 1 2)
 
