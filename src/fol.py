e89b8cce36afde7c14ef40aee1e731c2ae607152
Tianguang <penn.zht@gmail.com>
2024-01-27 01:34:13 +0100

    Add backbone.

---
+++ src/fol.py
@@ -0,0 +1,36 @@
+# FOL.
+# Currently ZOL (propositional) to bootstrap NDAG system.
+
+import expr
+import general  # for .match
+import toposort
+
+class TheoryError (Exception):
+    def __init__ (self, reason=None):
+        self.reason = reason
+    def __str__ (self):
+        return f'TheoryError: {self.reason}'
+    def __repr__ (self):
+        return f'TheoryError (reason = {repr(self.reason)})'
+
+def rules ():
+    return expr.data ('./fol.blue')
+
+def verify_per_step (graph):
+    '''Verifies a sorted graph step by step.'''
+    ...
+
+def verify (theory):
+    '''Verifies a theory.
+
+    A theory is a collection of definitions and links.'''
+    try:
+        graph = toposort (theory)
+        verify_per_step (graph)
+        return True
+    except TheoryError as e:
+        print (e)
+        return False
+
+print (expr.purr (rules ()))
+
---
+++ src/toposort.py
@@ -0,0 +1,9 @@
+# Toposort of a graph
+# Potential features:
+#     Automatically grouping nodes with affinity
+#     Assigning hints to order
+#     Incremental
+
+def toposort (theory):
+    pass
+
