b862c6f8efd0a665b7c343561a1094708997f8c6
Tianguang <penn.zht@gmail.com>
2024-02-03 19:19:43 +0100

    First "reactive" app.

--- js/run.js
+++ js/run.js
@@ -1,3 +1,14 @@
-document.getElementById('output').innerHTML = `
-    <h1>2 ^ 16 = ${2 ** 16}</h1>
-`;
+$ = (x) => document.getElementById(x);
+
+$('input').oninput = execute;
+
+window.onload = (e) => {
+    $('input').value = '2 ** 16';
+    execute(e);
+}
+
+function execute (e) {
+    inValue = $('input').value;
+    fn = new Function (`return ${inValue}`);
+    $('output').innerText = fn();
+}
