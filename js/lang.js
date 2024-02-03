227837219ff9498f7c353df2616162b4be0b06ef
Tianguang <penn.zht@gmail.com>
2024-02-03 20:40:36 +0100

    Remove test lines.

--- js/lang.js
+++ js/lang.js
@@ -1,11 +1,3 @@
-function strMult (string, copies) {
-    const ans = [];
-    for (var i = 0; i < copies; i++) ans.push (string);
-    return ans.join ('');
-}
-
-console.log (strMult ('hello, world!\n', 10));
-
 // Parsing a sexp, using only square brackets
 
 function parseSexp (input) {
