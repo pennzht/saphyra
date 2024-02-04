c5bc951399bd07a34085f3e63e86a90f4e1b8920
Tianguang <penn.zht@gmail.com>
2024-02-04 16:46:30 +0100

    Filling in `isValidDeriv`.

--- js/lang.js
+++ js/lang.js
@@ -32,31 +32,31 @@ function display (sexp) {
 function isValidStep (rule, ins, outs, subs = null) {
     const i = ins, o = outs;
     if (rule === 'and-intro') {
-        return i.length === 2 && equal (o, [['and', ...i]]);
+        return i.length === 2 && eq (o, [['and', ...i]]);
     } else if (rule === 'and-elim') {
-        return o.length === 2 && equal (i, [['and', ...o]]);
+        return o.length === 2 && eq (i, [['and', ...o]]);
     } else if (rule === 'or-intro-1') {
         return i.length === 1 && o.length === 1 &&
-            o[0][0] === 'or' && equal(o[0][1], i[0]);
+            o[0][0] === 'or' && eq(o[0][1], i[0]);
     } else if (rule === 'or-intro-2') {
         return i.length === 1 && o.length === 1 &&
-            o[0][0] === 'or' && equal(o[0][2], i[0]);
+            o[0][0] === 'or' && eq(o[0][2], i[0]);
     } else if (rule === 'or-elim') {
         return i.length === 3 && o.length === 1 &&
             i[0][0] === 'or' && i[1][0] === '->' && i[2][0] === '->' &&
-            equal(i[0][1], i[1][1]) && equal(i[0][2], i[2][1]) &&
-            equal(i[1][2], i[2][2]) && equal(i[1][2], o[0]);
+            eq(i[0][1], i[1][1]) && eq(i[0][2], i[2][1]) &&
+            eq(i[1][2], i[2][2]) && eq(i[1][2], o[0]);
     } else if (rule === 'false-elim') {
-        return equal (i, ['false']);
+        return eq (i, ['false']);
     } else if (rule === 'true-intro') {
-        return equal (o, ['true']);
+        return eq (o, ['true']);
     } else if (rule === 'mp') {
         return i.length === 2 && o.length === 1 &&
-            equal(i[0], i[1][1]) && i[1][0] === '->' &&
-            equal(i[1][2], o[0]);
+            eq(i[0], i[1][1]) && i[1][0] === '->' &&
+            eq(i[1][2], o[0]);
     } else if (rule === 'tnd') {
         const a = o[0][1];
-        return equal (o, [['or', a, ['->', a, 'false']]]);
+        return eq (o, [['or', a, ['->', a, 'false']]]);
     } else if (rule === 'impl-intro') {
         return true;  // TODO - judge
     } else if (rule === 'join') {
@@ -75,11 +75,29 @@ function isValidStepInAnyRule (ins, outs, subs = null) {
 /// If a sequence of items (in correct order) is a valid derivation.
 /// Applies to a system.
 function isValidDeriv (lines) {
-    /// TODO: fill in
+    const nodes = new Map();
+    const descs = new Map();  // Descendants
+    const ances = new Map();  // Ancestors
+    const reasons = new Map();  // Reasons for each step
+
+    const ans = {};  // Stores the judgment
+
+    for (const line of lines) {
+        if (line.length === 0) continue;
+        if (line[0] === 'comment') continue;
+
+        if (line[0] === 'node') {
+            // TODO
+        } else if (line[0] === 'derive') {
+            // TODO
+        } else if (line[0] === 'link') {
+            // TODO
+        }
+    }
 }
 
 /// If two sexps are equal.
-function equal (a, b) {
+function eq (a, b) {
     return JSON.stringify(a) === JSON.stringify(b);
 }
 
