79186887e597cb0dcf185b92b53a6ae880678a2a
Tianguang <penn.zht@gmail.com>
2024-09-16 00:36:07 +0200

    Fix importing.

--- js/export_import.js
+++ js/export_import.js
@@ -2,7 +2,7 @@ function exportState() {
     const tabs = [...state.tabs].map ((pair) => {
         const [name, [activeStep, ...steps]] = pair;
         const stepsTranslated = steps.map((s) => str(s));
-        return [name, [activeStep, stepsTranslated]];
+        return [name, [activeStep, ...stepsTranslated]];
     });
     const highlighted = [...state.highlighted];
     const json = JSON.stringify({
@@ -38,5 +38,13 @@ function importState(state) {
 function setEditorState(result) {
     console.log('Finished reading', result);
     const data = JSON.parse(result);
-    // TODO - continue
+
+    state.tabs = new Map(data.tabs.map ((pair) => {
+        const [name, [activeStep, ...stepsTranslated]] = pair;
+        const steps = stepsTranslated.map((s) => parseOne(s));
+        return [name, [activeStep, ...steps]];
+    }));
+    state.highlighted = new Set(data.highlighted);
+    state.currentTab = data.currentTab;
+    updateState();
 }
