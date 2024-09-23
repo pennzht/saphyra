64e02dd93b9f572d874adb10e6b9f8f4a3146d08
Tianguang <penn.zht@gmail.com>
2024-09-23 18:57:04 +0200

    Use good naming.

--- js/infix.js
+++ js/infix.js
@@ -22,17 +22,17 @@ PRECE = new Map([
 
 // Fn application is '@', and always requires parens (except when var)
 
-function _normalize_symbol (symbol) {
+function _normalizeSymbol (symbol) {
   const found = {'=>': ':', '∀': 'forall', '∃': 'exists'}[symbol];
   return found ? found : symbol;
 }
 
-function get_associativity (operator) {
+function getAssociativity (operator) {
   if (['^', 'forall', 'exists', ':', '->'].includes(operator)) return 'R';
   return 'L';
 }
 
-function infix_parse (string) {
+function infixParse (string) {
   // TODO0923
   const rawParse = parse(string);
 
@@ -43,11 +43,11 @@ function infix_parse (string) {
   // Compose result object
 }
 
-function infix_format (expr) {
-  return _infix_format_p (expr, /*parent*/ '()', /*prefix*/ null);
+function infixFormat (expr) {
+  return _infixFormatP (expr, /*parent*/ '()', /*prefix*/ null);
 }
 
-function _infix_format_p (expr, parent, prefix) {
+function _infixFormatP (expr, parent, prefix) {
   // prefix represents the relative position of a subexpression in the statement.
 
   const pf = isList(prefix) ? prefix : [];
@@ -89,15 +89,15 @@ function _infix_format_p (expr, parent, prefix) {
 
       if (isAtomic(head) && head !== ':') {
         return elem('span', {'data-sexp': str(obj), 'data-relpos': str(pf)}, [
-          _infix_format_p(a, head, [...pf, 1]),
-          _infix_format_p(head, head, [...pf, 0]),
-          _infix_format_p(b, head, [...pf, 2]),
+          _infixFormatP(a, head, [...pf, 1]),
+          _infixFormatP(head, head, [...pf, 0]),
+          _infixFormatP(b, head, [...pf, 2]),
         ])
       } else if (head === ':') {
         return elem('span', {'data-sexp': str(obj), 'data-relpos': str(pf)}, [
-          _infix_format_p(a, head, [...pf, 1]),
-          _infix_format_p('\u21a6', head, [...pf, 0]),
-          _infix_format_p(b, head, [...pf, 2]),
+          _infixFormatP(a, head, [...pf, 1]),
+          _infixFormatP('\u21a6', head, [...pf, 0]),
+          _infixFormatP(b, head, [...pf, 2]),
         ])
       }
     }
@@ -108,8 +108,8 @@ function _infix_format_p (expr, parent, prefix) {
 
       return elem('span', {'data-sexp': str(obj), 'data-relpos': str(pf)}, [
         text('∀'),
-        _infix_format_p(v, 'forall', [...pf, 1, 1]),
-        _infix_format_p(body, 'forall', [...pf, 1, 2]),
+        _infixFormatP(v, 'forall', [...pf, 1, 1]),
+        _infixFormatP(body, 'forall', [...pf, 1, 2]),
       ])
     }
 
@@ -119,8 +119,8 @@ function _infix_format_p (expr, parent, prefix) {
 
       return elem('span', {'data-sexp': str(obj), 'data-relpos': str(pf)}, [
         text('∃'),
-        _infix_format_p(v, 'exists', [...pf, 1, 1]),
-        _infix_format_p(body, 'exists', [...pf, 1, 2]),
+        _infixFormatP(v, 'exists', [...pf, 1, 1]),
+        _infixFormatP(body, 'exists', [...pf, 1, 2]),
       ])
     }
 
@@ -128,7 +128,7 @@ function _infix_format_p (expr, parent, prefix) {
     
     const subobjs = [];
     for (let i = 0; i < obj.length; i++) {
-      subobj = _infix_format_p(obj[i], '@', [...pf, i]);
+      subobj = _infixFormatP(obj[i], '@', [...pf, i]);
       subobjs.push(subobj);
     }
 
