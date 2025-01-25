e30b6ce06ada7f42bba89f72a8ff5b006ba30717
Tianguang <penn.zht@gmail.com>
2025-01-25 16:31:34 +0100

    Add polynomial reasoning.

--- js/polynomial.js
+++ js/polynomial.js
@@ -1,8 +1,35 @@
 Q = (...args) => document.querySelectorAll(...args);
 
+function _makeConsts(expr) {
+    if (typeof (expr) === 'string' && expr.match(/^[0-9]+$/g)) {
+        return parseInt (expr);
+    }
+    if (typeof (expr) === 'string') return expr;
+    return expr.map (_makeConsts);
+}
+
+function infixParseWConsts (expr) { return _makeConsts (infixParse (expr)); }
+
 $('text').oninput = (e) => {
     $('display').innerHTML = '';
-    $('display').appendChild (infixFormat (infixParse (
+    $('display').appendChild (infixFormat (infixParseWConsts (
         e.target.value
     )));
 }
+
+/*
+General format;
+expr = [+ term1 ... termN]
+term = [* coeff(nat) [^ base1 exp1] [^ base2 exp2] ... [^ baseN expN]]
+base, exp : expr
+
+sum normalization: combine like terms
+term normalization: combine like powers
+exponentiation normalization: move powers as high as possible; split out constant exponents
+    (a b) ^ n ⇒ a^n b^n
+    (a ^ n) ^ m ⇒ a ^ (nm)
+    a ^ n · a ^ m ⇒ a ^ (n+m)
+    a ^ (n+K) ⇒ a ^ n · a ^ K
+    expand (a + b) ^ K
+*/
+
